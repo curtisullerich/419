b46ac703280e4ba5ce386f562723a5d4e1c58aad
Curtis Ullerich <curtisullerich@gmail.com>
2013-02-25 23:03:27 -0600

    lab4

--- lab4/Cluster.java
+++ lab4/Cluster.java
@@ -13,7 +13,6 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import java.nio.ByteBuffer;
 
-
 /**
  * @author Curtis Ullerich <curtisu@iastate.edu>
  */
@@ -35,7 +34,7 @@ public class Cluster extends Configured implements Tool {
 		String temp2 =   "/users/curtisu/lab4/exp"+runnumber+"tmp2";
 		String output = "/users/curtisu/lab4/exp"+runnumber+"output";
 		
-		int reduce_tasks = 4;  // The number of reduce tasks that will be assigned to the job
+		int reduce_tasks = 2;  // The number of reduce tasks that will be assigned to the job
 		Configuration conf = new Configuration();
 		
 		// Create job for round 1
@@ -120,7 +119,7 @@ public class Cluster extends Configured implements Tool {
 	public static class Map_One extends Mapper<LongWritable, Text, Text, Text>  {
 
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException  {
-			int k = 4;
+			int k = 4; //the number of hash functions to use in the combined key
 			int hashes[] = new int[10];
 			String line = value.toString();
 
@@ -133,6 +132,7 @@ public class Cluster extends Configured implements Tool {
 				hashes[j] = Map_One.hash(firstShingle.getBytes(), j);
 			}
 
+			//hash all shingles
 			for (int i = line.indexOf('-')+1; i < line.length()-k+1; i++) {
 				String shingle = line.substring(i, i+k);
 				for (int j = 0; j < hashes.length; j++) {
@@ -143,6 +143,7 @@ public class Cluster extends Configured implements Tool {
 				}
 			}
 
+			//cat the hashes
 			String hashstring = "";
 			for (int s : hashes) {
 				hashstring += s + ",";
@@ -150,7 +151,7 @@ public class Cluster extends Configured implements Tool {
 
 			hashstring = hashstring.substring(0, hashstring.length() - 1);
 			// output
-			// hash0-hash1-hash2-hash3            documentID-oiusdASgFyHfNaAiNSuJlWJsMdfguygILFUGAFOIUAWGFouygfoiuagfuyagsdfiuG
+			// hash0,hash1,hash2,hash3            documentID-oiusdASgFyHfNaAiNSuJlWJsMdfguygILFUGAFOIUAWGFouygfoiuagfuyagsdfiuG
 			context.write(new Text(hashstring), value);//output the minhash set along with the original value
 		}
 		/*
@@ -158,65 +159,71 @@ public class Cluster extends Configured implements Tool {
 	 * to help with minhashing. New hash functions can be derived by using a 
 	 * new seed value.
 	 */
-	private static int hash(byte[] b_con, int i_seed){
+		private static int hash(byte[] b_con, int i_seed){
 
-		String content = new String(b_con);
-		//System.out.println("hashing with content = " + content);
-		int seed = i_seed;
-		int m = 0x5bd1e995;
-		int r = 24;
+			String content = new String(b_con);
+			//System.out.println("hashing with content = " + content);
+			int seed = i_seed;
+			int m = 0x5bd1e995;
+			int r = 24;
 
-		int len = content.length();
-		byte[] work_array = null;
+			int len = content.length();
+			byte[] work_array = null;
 
-		int h = seed ^ content.length();
+			int h = seed ^ content.length();
 
-		int offset = 0;
+			int offset = 0;
 
-		work_array = new byte[4];
-		while( len >= 4)
-		{
-			ByteBuffer buf = ByteBuffer.wrap(content.substring(offset, offset + 4).getBytes());
+			work_array = new byte[4];
+			while( len >= 4)
+			{
+				ByteBuffer buf = ByteBuffer.wrap(content.substring(offset, offset + 4).getBytes());
 
-			int k = buf.getInt();
-			k = k * m;
-			k ^= k >> r;
-			k *= m;
+				int k = buf.getInt();
+				k = k * m;
+				k ^= k >> r;
+				k *= m;
+				h *= m;
+				h ^= k;
+
+				offset += 4;
+				len -= 4;
+			}
+
+			switch(len){
+				case 3: h ^= work_array[2] << 16;
+				case 2: h ^= work_array[1] << 8;
+				case 1: h ^= work_array[0];
+				h *= m;
+			}
+
+			h ^= h >> 13;
 			h *= m;
-			h ^= k;
+			h ^= h >> 15;
 
-			offset += 4;
-			len -= 4;
+			return h;
 		}
-
-		switch(len){
-			case 3: h ^= work_array[2] << 16;
-			case 2: h ^= work_array[1] << 8;
-			case 1: h ^= work_array[0];
-			h *= m;
-		}
-
-		h ^= h >> 13;
-		h *= m;
-		h ^= h >> 15;
-
-		return h;
-	}
 	}
 	
 	public static class Reduce_One extends Reducer<Text, Text, Text, Text>  {
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException  {
 			String ids = "";
 			String doc = "";
+
 			for (Text val : values) {
 				String line = val.toString();
+
+				//if we're looking at the first element
 				if (ids.equals("")) {
 					doc += line.substring(line.indexOf('-')+1);
 				}
+
+				//grab all the ids and shove them together
 				ids += line.substring(0, line.indexOf('-'));
 				//System.out.println("adding id: " + line.substring(0, line.indexOf('-')));
 				ids += ",";
 			}
+			//remove trailing comma
 			ids = ids.substring(0, ids.length()-1);
 			//System.out.println("all ids: " + ids);
 			//keep doc IDs and one sample
@@ -230,6 +237,7 @@ public class Cluster extends Configured implements Tool {
 	public static class Map_Two extends Mapper<LongWritable, Text, Text, Text>  {
 		Text t = new Text("key");
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			//send to the same reducer
 			context.write(t, value);
 			//System.out.println("mappin value: " + value.toString());
 		}
@@ -240,11 +248,13 @@ public class Cluster extends Configured implements Tool {
 			System.out.println("starting reduce 2 : -0");
 			List<Datum> data = new ArrayList<Datum>();
 			
+			//these values can be used to define various thresholds for halting
 			int count = 0;
 			int sum = 0;
+			int imax = 0;
 			for (Text t : values) {
-				//System.out.println("key : " + key.toString());
-				//System.out.println("value: " + t.toString());
+
+				//parse the file
 				String line = t.toString();
 				String parts[] = line.split("\t");
 				String hashes[] = parts[0].split(",");
@@ -254,31 +264,57 @@ public class Cluster extends Configured implements Tool {
 				data.add(d);
 				count++;
 				sum += ids.length;
+				if (ids.length > imax) {
+					imax = ids.length;
+				}
 
 				//context.write(key, new Text(ids.length+""));
 			}
+
 			context.progress();
 			System.out.println("reduce 2 : -1. Finished creating data.");
+
+			//this is the number of hash functions used in map_one
+			int numhashes = data.get(0).hashes.length;
+
+			//allow clusters with this ratio of matching hashes to combine
+			//note that this loses information in the values of the hashes of one of the clusters
+			double ratio = 1./2.;
+			for (int i = 0; i < data.size()-1; i++) {
+				for (int j = i+1; j < data.size(); j++) {
+					if (i != j && data.get(i).matchesBy((int)(numhashes*ratio), data.get(j))) {
+						data.get(i).ids.addAll(data.get(j).ids);
+						data.remove(j);
+						j--;
+					}
+				}
+			}
+
 			double average = sum/(float)count;
 			List<Datum> misfits = new ArrayList<Datum>();
 			List<Datum> keepers = new ArrayList<Datum>();
+
+			//divide clusters into misfits and keepers based on the size of the maximum cluster size
+			double percentOfMax = 0.7;
 			for (int i = 0; i < data.size(); i++) {
-				if (data.get(i).ids.size() <= average*.7) {
+				if (data.get(i).ids.size() <= imax*percentOfMax) {
 					misfits.add(data.get(i));
 				} else {
 					keepers.add(data.get(i));
 				}
 			}
 			System.out.println("reduce 2 : -2. Finished splitting misfits and keepers.");
+			System.out.println("there are " + misfits.size() + " misfits and " + keepers.size() + " keepers.");
 
 			context.progress();
 			
-			//data = null;
-			
+			int num = 0;
+			//slowly relax the similarity requirements for combining a misfit into a keeper
 			for (int i = 0; i < misfits.size(); i++) {
 				kbr: for (int k = 4; k > 0; k--) {
 					for (int j = 0; j < keepers.size(); j++) {
 						if (misfits.get(i).matchesBy(k, keepers.get(j))) {
+							System.out.println("found a home for misfit " + num++ + " with k=" + k);
 							keepers.get(j).ids.addAll(misfits.get(i).ids);
 							misfits.remove(i);
 							i--;
@@ -287,9 +323,13 @@ public class Cluster extends Configured implements Tool {
 					}
 				}
 			}
-			System.out.println("starting reduce 2 : -3. Finished attempting to merge misfits into keepers.");
+			System.out.println("reduce 2 : -3. Finished attempting to merge misfits into keepers.");
+			System.out.println("there are still " + misfits.size() + " misfit.");
 			context.progress();
 
+			//as a last-ditch effort to find a home for misfits, stick them in with the cluster
+			//that provides the highest jaccard similarity. This really shouldn't happen
+			//on this data set.
 			while (misfits.size() > 0) {
 				double max = -1;
 				int index = -1;
@@ -306,16 +346,30 @@ public class Cluster extends Configured implements Tool {
 				misfits.remove(misfits.size()-1);
 			}
 			context.progress();
+			System.out.println("reduce 2 : -4. Finished jaccarding misfits.");
+			
 
 			for (Datum d : keepers) {
-				context.write(new Text(d.ids.size() + " " + d.ids.toString()), new Text(d.doc));
+				System.out.println(d.ids.size() + "\t" + d.ids.toString());
+				String s= d.ids.toString();
+				context.write(new Text(s.substring(1, s.length()-1)), new Text(d.doc));
 			}
+
+			System.out.println("reduce 2: -5. Done.");
 		}
 
+		/**
+     * A class to contain a particular cluster and compute similarity between other clusters.
+     *
+		 */
 		public class Datum {
+			//a representative document
 			public String doc;
+			//the list of minhashes for this cluster
 			public String[] hashes;
+			//the list of ids in this cluster
 			public ArrayList<String> ids;
+
 			public Datum(String doc, String[] hashes, String[] ids) {
 				this.doc = doc;
 				this.hashes = hashes;
@@ -323,6 +377,7 @@ public class Cluster extends Configured implements Tool {
 				this.ids.addAll(Arrays.asList(ids));
 			}
 
+			//return the jaccard similarity between this and other
 			public double jaccard(Datum other) {
 				Set<String> first = new HashSet<String>();
 				Set<String> union = new HashSet<String>();
@@ -341,13 +396,14 @@ public class Cluster extends Configured implements Tool {
 				return jac;
 			}
 
+			//return true if this and other contain at least threshold common minhashes
 			public boolean matchesBy(int threshold, Datum other) {
 				if (this.hashes.length != other.hashes.length) {
 					throw new IllegalArgumentException("Data had different number of hashes.");
 				}
 				int count = 0;
 				for (int i = 0; i < hashes.length; i++) {
-					if (this.hashes[i] == other.hashes[i]) {
+					if (this.hashes[i].equals(other.hashes[i])) {
 						count++;
 					}
 				}
