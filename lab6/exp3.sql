ef35e1e28541266e06a9942ffa109fe596214b6d
Curtis Ullerich <curtisullerich@gmail.com>
2013-03-20 18:05:29 -0400

    removed old files

--- lab6/exp3.sql
+++
@@ -1,7 +0,0 @@
-CREATE EXTERNAL TABLE curtis.iptrace  (src string, bracket string, dest string, protocol string, port string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ' ' LOCATION '/users/curtisu/Lab6/Exp3/ip_trace';
-CREATE EXTERNAL TABLE curtis.rawblock (time string, id string, src string, bracket string, dest string, protocol string, dependent string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ' ' LOCATION '/users/curtisu/Lab6/Exp3/raw_block';
-
-select distinct src, count(src) as c from curtis.rawblock group by src sort by c desc;
-
-
-
