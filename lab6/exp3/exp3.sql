61fb3d6308dec45df0d802bac108dcbe58481616
Curtis Ullerich <curtisullerich@gmail.com>
2013-03-20 19:51:37 -0500

    added exp3 files

--- lab6/exp3/exp3.sql
+++ lab6/exp3/exp3.sql
@@ -1,7 +1,19 @@
-CREATE EXTERNAL TABLE curtis.iptrace  (src string, bracket string, dest string, protocol string, port string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ' ' LOCATION '/users/curtisu/Lab6/Exp3/ip_trace';
-CREATE EXTERNAL TABLE curtis.rawblock (time string, id string, src string, bracket string, dest string, protocol string, dependent string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ' ' LOCATION '/users/curtisu/Lab6/Exp3/raw_block';
+--<Time> <Connection ID> <Source IP> <Destination IP> “Blocked”
+--<id> <blocked>
+use curtis;
+CREATE EXTERNAL TABLE curtis.iptrace  (time string, id bigint, src string, dest string, status string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ' ' LOCATION '/users/curtisu/Lab6/Exp3/ip_trace';
+CREATE EXTERNAL TABLE curtis.rawblock (id bigint, status string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ' ' LOCATION '/users/curtisu/Lab6/Exp3/raw_block';
 
-select distinct src, count(src) as c from curtis.rawblock group by src sort by c desc;
+
+select src from curtis.rawblock;
+  
+
+SELECT distinct src
+FROM (
+  select src, count(1) as c from curtis.rawblock group by src sort by c desc
+)
+
+;
 
 
 
