9943d13b15976cc32ee610365e4964b4f3f8398d
Curtis Ullerich <curtisullerich@gmail.com>
2013-03-20 20:50:19 -0500

    added exp3 files

--- lab6/exp3/exp3.sql
+++ lab6/exp3/exp3.sql
@@ -1,19 +1,22 @@
---<Time> <Connection ID> <Source IP> <Destination IP> “Blocked”
---<id> <blocked>
 use curtis;
-CREATE EXTERNAL TABLE curtis.iptrace  (time string, id bigint, src string, dest string, status string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ' ' LOCATION '/users/curtisu/Lab6/Exp3/ip_trace';
-CREATE EXTERNAL TABLE curtis.rawblock (id bigint, status string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ' ' LOCATION '/users/curtisu/Lab6/Exp3/raw_block';
 
+--part a
+--<Time> <Connection ID> <Source IP> <Destination IP> “Blocked”
+CREATE EXTERNAL TABLE curtis.iptrace  (time string, id bigint, src string, bracket string, dest string, protocol string, port string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ' ' LOCATION '/users/curtisu/datasets/iptrace/';
+--'/users/curtisu/Lab6/Exp3/ip_trace';
 
-select src from curtis.rawblock;
+--<id> <blocked>
+CREATE EXTERNAL TABLE curtis.rawblock (id bigint, status string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ' ' LOCATION '/users/curtisu/datasets/rawblock';
+--'/users/curtisu/Lab6/Exp3/raw_block';
   
+--part b
+select src, count(1) as c 
+from (select * from rawblock join iptrace on (rawblock.id = iptrace.id and rawblock.status = 'Blocked')) blocks
+group by src sort by c desc;
 
-SELECT distinct src
-FROM (
-  select src, count(1) as c from curtis.rawblock group by src sort by c desc
-)
-
-;
-
+--part c
+select port, count(1) as c 
+from (select * from rawblock join iptrace on (rawblock.id = iptrace.id and rawblock.status = 'Blocked')) blocks
+group by port sort by c desc limit 5;
 
 
