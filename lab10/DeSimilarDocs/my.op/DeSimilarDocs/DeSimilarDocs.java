72a2750b189315aeb13cae920c0478cc46255104
Curtis Ullerich <curtisullerich@gmail.com>
2013-04-24 16:16:13 -0400

    compiles\!

--- lab10/DeSimilarDocs/my.op/DeSimilarDocs/DeSimilarDocs.java
+++ lab10/DeSimilarDocs/my.op/DeSimilarDocs/DeSimilarDocs.java
@@ -4,7 +4,7 @@ import com.ibm.streams.operator.OutputTuple;
 import com.ibm.streams.operator.StreamingInput;
 import com.ibm.streams.operator.Tuple;
 import com.ibm.streams.operator.StreamingOutput;
-public class Test extends AbstractOperator {
+public class DeSimilarDocs extends AbstractOperator {
   @Override
   public void initialize(OperatorContext context) throws Exception {
     super.initialize(context);
@@ -13,7 +13,6 @@ public class Test extends AbstractOperator {
     final StreamingOutput<OutputTuple> output = getOutput(0);
     String tstring = tuple.getString("time");
     String nstring = tuple.getString("name");
-  
     
     output.submit(tuple);
   }
