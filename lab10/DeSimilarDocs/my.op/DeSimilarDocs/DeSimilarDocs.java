1490a7b91a65beeeb54c9668c3b4eb4a41b300be
Curtis Ullerich <curtisullerich@gmail.com>
2013-05-01 17:32:26 -0500

    java

--- lab10/DeSimilarDocs/my.op/DeSimilarDocs/DeSimilarDocs.java
+++ lab10/DeSimilarDocs/my.op/DeSimilarDocs/DeSimilarDocs.java
@@ -192,106 +192,48 @@ public class DeSimilarDocs extends AbstractOperator {
   * to help with minhashing. New hash functions can be derived by using a 
   * new seed value.
   */
-    private static int hash(byte[] b_con, int i_seed){
+  private static int hash(byte[] b_con, int i_seed){
 
-      String content = new String(b_con);
-      //System.out.println("hashing with content = " + content);
-      int seed = i_seed;
-      int m = 0x5bd1e995;
-      int r = 24;
+    String content = new String(b_con);
+    //System.out.println("hashing with content = " + content);
+    int seed = i_seed;
+    int m = 0x5bd1e995;
+    int r = 24;
 
-      int len = content.length();
-      byte[] work_array = null;
+    int len = content.length();
+    byte[] work_array = null;
 
-      int h = seed ^ content.length();
+    int h = seed ^ content.length();
 
-      int offset = 0;
+    int offset = 0;
 
-      work_array = new byte[4];
-      while( len >= 4)
-      {
-        ByteBuffer buf = ByteBuffer.wrap(content.substring(offset, offset + 4).getBytes());
+    work_array = new byte[4];
+    while( len >= 4)
+    {
+      ByteBuffer buf = ByteBuffer.wrap(content.substring(offset, offset + 4).getBytes());
 
-        int k = buf.getInt();
-        k = k * m;
-        k ^= k >> r;
-        k *= m;
-        h *= m;
-        h ^= k;
-
-        offset += 4;
-        len -= 4;
-      }
-
-      switch(len){
-        case 3: h ^= work_array[2] << 16;
-        case 2: h ^= work_array[1] << 8;
-        case 1: h ^= work_array[0];
-        h *= m;
-      }
-
-      h ^= h >> 13;
+      int k = buf.getInt();
+      k = k * m;
+      k ^= k >> r;
+      k *= m;
       h *= m;
-      h ^= h >> 15;
+      h ^= k;
 
-      return h;
+      offset += 4;
+      len -= 4;
     }
+
+    switch(len){
+      case 3: h ^= work_array[2] << 16;
+      case 2: h ^= work_array[1] << 8;
+      case 1: h ^= work_array[0];
+      h *= m;
+    }
+
+    h ^= h >> 13;
+    h *= m;
+    h ^= h >> 15;
+
+    return h;
   }
-  /**
-   * A class to contain a particular cluster and compute similarity between other clusters.
-   *
-   */
-  public class Datum {
-    //a representative document
-    public String doc;
-    //the list of minhashes for this cluster
-    public String[] hashes;
-    //the list of ids in this cluster
-    public ArrayList<String> ids;
-
-    public Datum(String doc, String[] hashes, String[] ids) {
-      this.doc = doc;
-      this.hashes = hashes;
-      this.ids = new ArrayList<String>();
-      this.ids.addAll(Arrays.asList(ids));
-    }
-
-    //return the jaccard similarity between this and other
-    public double jaccard(Datum other) {
-      Set<String> first = new HashSet<String>();
-      Set<String> union = new HashSet<String>();
-      int k = 9;
-      for (int i = 0; i < this.doc.length()-k; i++) {
-        first.add(this.doc.substring(i, i+k));
-        union.add(this.doc.substring(i, i+k));
-      }
-      for (int i = 0; i < other.doc.length()-k; i++) {
-        union.add(other.doc.substring(i, i+k));
-        if (!first.contains(other.doc.substring(i, i+k))) {
-          first.remove(other.doc.substring(i, i+k));
-        }
-      }
-      double jac = (double) first.size() / (double) union.size();
-      return jac;
-    }
-
-    //return true if this and other contain at least threshold common minhashes
-    public boolean matchesBy(int threshold, Datum other) {
-      if (this.hashes.length != other.hashes.length) {
-        throw new IllegalArgumentException("Data had different number of hashes.");
-      }
-      int count = 0;
-      for (int i = 0; i < hashes.length; i++) {
-        if (this.hashes[i].equals(other.hashes[i])) {
-          count++;
-        }
-      }
-      if (count >= threshold) {
-        return true;
-      }
-      return false;
-    }
-
-  }
-
 }
