0f2813f9d8eefd09cc60bb600be93125fc92261b
Curtis Ullerich <curtisullerich@gmail.com>
2013-05-01 21:11:25 -0500

    java

--- lab10/DeSimilarDocs/my.op/DeSimilarDocs/DeSimilarDocs.java
+++ lab10/DeSimilarDocs/my.op/DeSimilarDocs/DeSimilarDocs.java
@@ -56,7 +56,7 @@ public class DeSimilarDocs extends AbstractOperator {
     // name is a filename, so read it in and put it in a buffer
     byte[] file = readFile(nstring);
     String line = new String(file);
-    int hashes[] = new int[k];
+    int hashes[] = new int[4];
     String firstShingle = line.substring(0, k);
     //System.out.println("first shingle: " + firstShingle);
     for (int j = 0; j < hashes.length; j++) {
@@ -67,11 +67,11 @@ public class DeSimilarDocs extends AbstractOperator {
     for (int i = 0; i < line.length()-k+1; i++) {
       String shingle = line.substring(i, i+k);
       for (int j = 0; j < hashes.length; j++) {
-        //int h = hash(shingle.getBytes(), j);
+        int h = hash(shingle.getBytes(), j);
         //always keep the mins
-        //if (h < hashes[j]) {
-        //  hashes[j] = h;
-        //}
+        if (h < hashes[j]) {
+          hashes[j] = h;
+        }
       }
     }
 
